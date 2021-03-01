8a10f0dc53ed9b7e03bbb76d5610459dc67f8024
EvgeniyaUsh <evgeniya.ush20@gmail.com>
2021-03-01 14:44:23 +0300

    add test for task02

--- homework1/task02/fibonacci/fib.py
+++ homework1/task02/fibonacci/fib.py
@@ -2,7 +2,8 @@ from collections.abc import Sequence
 
 
 def check_fibonacci(data: Sequence[int]) -> bool:
-    assert len(data) >= 3, "it's not a fib sequence!"
+    if len(data) < 3:
+        return False
     a, b, c = data[0], data[1], data[2]
     while len(data):
         if a + b != c:
---
+++ homework1/task02/tests/test_fibonacci.py
@@ -0,0 +1,44 @@
+from collections import Sequence
+
+import pytest
+
+from task02.fibonacci.fib import check_fibonacci
+
+data = [
+    0,
+    1,
+    1,
+    2,
+    3,
+    5,
+    8,
+    13,
+    21,
+    34,
+    55,
+    89,
+    144,
+    233,
+    377,
+    610,
+    987,
+    1597,
+    2584,
+    4181,
+    6765,
+]
+
+
+@pytest.mark.parametrize(
+    ["value", "expected_result"],
+    [
+        ([0, 1, 1], True),
+        (data, True),
+        ([0, 1, 1, 2, 4], False),
+        ([], False),
+    ],
+)
+def test_power_of_2(value: Sequence[int], expected_result: bool):
+    actual_result = check_fibonacci(value)
+
+    assert actual_result == expected_result
