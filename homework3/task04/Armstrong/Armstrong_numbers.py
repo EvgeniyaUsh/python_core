9cb061093af82514bf60b6ab89e7e3310d9da47b
EvgeniyaUsh <evgeniya.ush20@gmail.com>
2021-03-12 22:16:22 +0300

    add task4 from hw3

--- homework3/task04/Armstrong/Armstrong_numbers.py
+++ homework3/task04/Armstrong/Armstrong_numbers.py
@@ -1,4 +1,9 @@
 def is_armstrong(number: int) -> bool:
+    """
+    a function in functional style that determines whether a number is an Armstrong number.
+    :param number:
+    :return: True if yes and vice versa
+    """
     degree = len(str(number))
     list_of_numbers = list(map(int, str(number)))
     numbers_to_degree = list(map(lambda x: x ** degree, list_of_numbers))
--- homework3/task04/tests/test_Armstrong_number.py
+++ homework3/task04/tests/test_Armstrong_number.py
@@ -1,16 +1,18 @@
 import pytest
 
-from task04.Armstrong.Armstrong_number import is_armstrong
+from task04.Armstrong.Armstrong_numbers import is_armstrong
 
 
+# test for Armstrong/Armstrong_numbers.py for funk is_armstrong()
 @pytest.mark.parametrize(
     ["value", "expected_result"],
     [
         (9, True),
         (10, False),
         (153, True),
+        (0, True),
     ],
 )
-def test_check_is_a_number_is_an_Armstrong_munber(value, expected_result):
+def test_check_is_a_number_is_an_armstrong_number(value, expected_result):
     actual_result = is_armstrong(value)
     assert actual_result == expected_result
