1a94e59987f08f8e74735c82b2c21a7db3c4c056
EvgeniyaUsh <evgeniya.ush20@gmail.com>
2021-03-16 22:01:23 +0300

    add task05 from hw4

--- homework4/task04/fizzBuzz/FizzBuzz_numbers.py
+++ homework4/task04/fizzBuzz/FizzBuzz_numbers.py
@@ -4,15 +4,15 @@ from typing import List
 def fizzbuzz(n: int) -> List[str]:
     """
     a function that takes an N number as input and returns N FizzBuzzaaa numbers.
-    :param n: list of numbers
+    :param n: the number of numbers in the list
     :return: 'fizz' if the number is divisible by three,
     'buzz' if the number is divisible by five,
     otherwise the numbers do not change.
 
     >>> fizzbuzz(5)
     ['1', '2', 'fizz', '4', 'buzz']
-    >>> fizzbuzz(10)
-    ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz']
+    >>> fizzbuzz(16)
+    ['1', '2', 'fizz', '4', 'buzz', 'fizz', '7', '8', 'fizz', 'buzz', '11', 'fizz', '13', '14', 'fizz buzz', '16']
     >>> fizzbuzz("string")
     Traceback (most recent call last):
         ...
@@ -21,10 +21,12 @@ def fizzbuzz(n: int) -> List[str]:
     fizzbuzz_nums = []
     try:
         for i in range(1, n + 1):
-            if i % 3 == 0:
-                i = "fizz"
+            if i % 15 == 0:
+                i = "fizz buzz"
             elif i % 5 == 0:
                 i = "buzz"
+            elif i % 3 == 0:
+                i = "fizz"
             fizzbuzz_nums.append(str(i))
     except Exception:
         raise ValueError
---
+++ homework4/task05/fizzBuzz_numpy/FizzBuzz_numpy.py
@@ -0,0 +1,37 @@
+import numpy as np
+
+
+def fizzbuzz(n):
+    """
+    FizzBuzz on numpy.
+    :param n: the number of numbers in the list
+    :return: "Fizz" if the number is divisible by three,
+    "Buzz" if the number is divisible by five,
+    "Fizz Buzz" if the number is divisible by,
+     otherwise the numbers do not change.
+
+     >>> fizzbuzz(5)
+    [1, 2, 'Fizz', 4, 'Buzz']
+    >>> fizzbuzz(16)
+    [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'Fizz Buzz', 16]
+    >>> fizzbuzz("string")
+    Traceback (most recent call last):
+        ...
+    ValueError
+    """
+    try:
+        fb = np.arange(n + 1, dtype="object")
+        fb[::3] = "Fizz"
+        fb[::5] = "Buzz"
+        fb[::15] = "Fizz Buzz"
+    except Exception:
+        raise ValueError
+    return list(fb[1:])
+
+
+print(fizzbuzz(16))
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
