8dc1465fb5d9bcadcc9cd6d441aee3fd5949a1ea
EvgeniyaUsh <evgeniya.ush20@gmail.com>
2021-04-12 16:19:37 +0300

    add hw9

--- homework9/task1/hw1.py
+++ homework9/task1/hw1.py
@@ -1,3 +1,20 @@
+"""
+Write a function that merges integer from sorted files and returns an iterator
+file1.txt:
+1
+3
+5
+file2.txt:
+2
+4
+6
+>>> list(merge_sorted_files(["file1.txt", "file2.txt"]))
+[1, 2, 3, 4, 5, 6]
+
+>>> list(merge_sorted_files(["file1.txt", "file2.txt", "file3.txt"]))
+[1, 2, 3, 4, 5, 6, 7, 8, 9]
+"""
+
 from contextlib import ExitStack
 from itertools import chain
 from pathlib import Path
@@ -6,11 +23,9 @@ from typing import Iterator, List, Union
 
 def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
     """
-    >>> list(merge_sorted_files(["file1.txt", "file2.txt"]))
-    [1, 2, 3, 4, 5, 6]
-
-    >>> list(merge_sorted_files(["file1.txt", "file2.txt", "file3.txt"]))
-    [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    the function creates a list from the contents of several files in a directory
+    :param file_list:
+    :return:
     """
     with ExitStack() as stack:
         streams = (stack.enter_context(open(fname)) for fname in file_list)
@@ -19,6 +34,11 @@ def merge_sorted_files(file_list: List[Union[Path, str]]) -> Iterator:
 
 
 def merge_sorted_iterables(iters):
+    """
+    the function makes a flat sheet int and returns a sorted generator
+    :param iters:
+    :return:
+    """
     flat_list = list(chain(*iters))
     sorted_int_list = sorted(int(item) for item in flat_list)
     yield from sorted_int_list
