cdc296866def6e98d15ab1bebb21d6e5b698863b
EvgeniyaUsh <evgeniya.ush20@gmail.com>
2021-04-23 15:35:05 +0300

    add hw9

--- homework9/tests/test_sum_tokens.py
+++ homework9/tests/test_sum_tokens.py
@@ -2,7 +2,7 @@ from homework9.task3.sum_tokens import universal_file_counter
 
 
 def test_universal_file_counter_test_file_txt_6():
-    assert universal_file_counter("homework9/tests/", "txt") == 6
+    assert universal_file_counter("homework9/tests/test_file", "txt") == 6
 
 
 def test_universal_file_counter_test_file_txt_3():
